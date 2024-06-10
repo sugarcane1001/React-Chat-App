bfc600773513b69a3780d6b55246df5b87231d67
Zian <hasanzian6@gmail.com>
2024-06-10 17:55:35 -0400

    Added login UI

--- components/Login.js
+++ components/Login.js
@@ -1,3 +1,41 @@
+import Head from "next/head";
+import { ChatIcon } from "@chakra-ui/icons";
+import { Box, Button, Center, Stack } from "@chakra-ui/react";
+
 export default function Login (){
-    return "login"
+    return (
+        <>
+        
+            <Head>
+                <title>Login</title>
+            </Head>
+
+            <Center
+                h="100vh"
+            >
+                <Stack
+                    align="center"
+                    bgColor="gray.600"
+                    p={16}
+                    rounded = "3xl"
+                    spacing={12}
+                    boxShadow="md"
+                >
+                    <Box
+                        bgColor="blue.500"
+                        w="fit-content"
+                        p={5}
+                        rounded="3xl"
+                        boxShadow="md"
+                    >
+                        <ChatIcon w="100px" h="100px" color="white"/>
+                    </Box>
+                    
+                    <Button boxShadow="md">Google Sign In</Button>
+                </Stack>
+                
+            </Center>
+            
+        </>
+    )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import Login from "../components/Login";
 
 export default function App({ Component, pageProps }) {
   //return <Component {...pageProps} />;
 
-  return <Login />
+  return (
+  <ChakraProvider>
+    <Login />
+  </ChakraProvider>
+  )
 }
