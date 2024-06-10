d284b1ce6fd4f8ced5b82c09a81be8c48537e375
Zian <hasanzian6@gmail.com>
2024-06-10 18:44:19 -0400

    Added sidebar UI

---
+++ components/Sidebar.js
@@ -0,0 +1,64 @@
+import { ArrowLeftIcon } from "@chakra-ui/icons";
+import { Avatar, Button, Flex, IconButton, Text } from "@chakra-ui/react";
+import { px } from "framer-motion";
+
+const Chat = () => {
+    return (
+        <Flex padding={3} align="center" _hover={{bg: "gray.100", cursor:"pointer"}}>
+            <Avatar src="" marginEnd={3}/>
+            <Text>user@gmail.com</Text>
+        </Flex>
+    )
+}
+
+export default function Sidebar() {
+    return (
+        <Flex
+            //bg="green.100"
+            w="300px"
+            h="100vh"
+            borderEnd="1.5px solid"
+            borderColor="grey.200"
+            direction="column"
+        >
+            <Flex
+                h="81px"
+                w="100%"
+                //bgColor="red.100"
+                align="center"
+                justifyContent="space-between"
+                padding={3}
+            >
+                <Flex align="center">
+                    <Avatar src="" marginEnd={3}/>
+                    <Text>Name</Text>   
+                </Flex>
+
+                <IconButton size="sm" rounded="xl" icon={<ArrowLeftIcon/>}/>
+            </Flex>
+
+            <Button m={5} p={4}>New Chat</Button>
+
+            <Flex overflowX="scroll" direction="column" /*sx={{scrollbarWidth: "auto"}}*/>
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+                <Chat />
+            </Flex>
+            
+        </Flex>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Login from "../components/Login";
+import Sidebar from "../components/Sidebar";
 
 export default function App({ Component, pageProps }) {
   //return <Component {...pageProps} />;
 
   return (
   <ChakraProvider>
-    <Login />
+    {/*<Login />*/}
+    <Sidebar/>
   </ChakraProvider>
   )
 }
