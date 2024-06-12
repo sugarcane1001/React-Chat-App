355ff0884dc98638f5d093f7346e271a337d1ae7
Zian <hasanzian6@gmail.com>
2024-06-12 01:54:30 -0400

    finished sidebar DB functionality

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,22 +5,38 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebaseconfig";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollection } from 'react-firebase-hooks/firestore';
-import { collection } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebaseconfig";
 import getOtherEmail from "../utils/getOtherEmail";
+import { useRouter } from "next/router";
 
 
 export default function Sidebar() {
     const [user] = useAuthState(auth);
     const [snapshot, loading, error] = useCollection(collection(db, "chats"));
     const chats = snapshot?.docs.map(doc => ({id: doc.id, ...doc.data()}));
-    
+    const router = useRouter();
+
+    const redirect = (id) => {
+        router.push(`/chat/${id}`);
+    }
+
+    const chatExists = email => chats?.find(chat => (chat.users.includes(user.email) && chat.users.includes(email)));
+
+    const newChat = async () => {
+        const input = prompt("Enter email of receipient");
+        
+        if(!chatExists(input) && (input != user.email)){
+            await addDoc(collection(db, "chats"), { users: [user.email, input] });
+        }
+    }
+
     const chatList = () => {
         return (
             chats?.filter(chat => chat.users.includes(user.email))
             .map(
                 chat => 
-                    <Flex key={Math.random()} padding={3} align="center" _hover={{bg: "gray.100", cursor:"pointer"}}>
+                    <Flex key={Math.random()} padding={3} align="center" _hover={{bg: "gray.100", cursor:"pointer"}} onClick={() => redirect(chat.id)}>
                         <Avatar src="" marginEnd={3}/>
                         <Text>{getOtherEmail(chat.users, user)}</Text>
                     </Flex>
@@ -55,7 +71,7 @@ export default function Sidebar() {
                 <IconButton size="sm" rounded="xl" icon={<ArrowLeftIcon/>} onClick={() => signOut(auth)}/>
             </Flex>
 
-            <Button m={5} p={4}>New Chat</Button>
+            <Button m={5} p={4} onClick={() => newChat()}>New Chat</Button>
 
             <Flex overflowX="scroll" direction="column" flex={1} /*sx={{scrollbarWidth: "auto"}}*/>
                 {chatList()}
